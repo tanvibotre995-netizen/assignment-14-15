3a620d12c9e65c85163542b777dffec13f221c1b
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-05 11:38:17 +0530

    Create Q9.py

---
+++ Q9.py
@@ -0,0 +1,12 @@
+Multiplication= lambda No1,No2:(No1*No2)
+
+def main():
+    value1= int(input("enter number:"))
+    value2 =int(input("enter number:"))
+
+    ret = Add(value1,value2)
+
+    print("Multiplication is",ret)
+
+if __name__== "__main__":
+    main()
