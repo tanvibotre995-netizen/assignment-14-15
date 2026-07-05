48e48161455bae215812dc7c39129dca04772223
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-05 11:33:56 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,13 @@
+square = lambda n:n * n
+
+def main():
+    value = int(input("enter number:"))
+
+    ret = square(value)
+
+    print("square of number",ret)
+    
+
+if __name__ == "__main__":
+    main()
+
