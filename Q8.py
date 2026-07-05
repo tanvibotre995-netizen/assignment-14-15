6867256acfe868c9d79f44eaf672e7acca66d5da
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-05 11:37:30 +0530

    Create Q8.py

---
+++ Q8.py
@@ -0,0 +1,12 @@
+Add = lambda No1,No2:(No1+No2)
+
+def main():
+    value1= int(input("enter number:"))
+    value2 =int(input("enter number:"))
+
+    ret = Add(value1,value2)
+
+    print("addition is",ret)
+
+if __name__== "__main__":
+    main()
